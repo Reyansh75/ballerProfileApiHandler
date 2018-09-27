6ecb2c3e98c1db7e10d6c25e7b0f65049d625942
Reyansh75 <reynash.olapeko@gmail.com>
2018-09-27 12:42:38 +0200

    Edit api response

--- controllers/players/update.js
+++ controllers/players/update.js
@@ -6,14 +6,14 @@ module.exports = async (req, res) => {
   try {
     const { headers, body, params } = req;
 
-    const player = await axios({
+    await axios({
       method: 'patch',
       url: paths.build(paths.api.v1.API_PLAYERS_ID, params.id),
       headers: { Authorization: headers.authorization },
       data: body
     });
 
-    return res.sendStatus(200).end();
+    return res.status(200).json({ message: 'Successfully updated' }).end();
   } catch (error) {
     console.error(error);
   }
